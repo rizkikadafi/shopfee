f45864154be2946fa134c2867a317609e2736456
beststarr <119686833+beststarr@users.noreply.github.com>
2023-12-02 16:49:08 +0700

    Update profile_page.dart

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shopfee/views/themes/color_scheme.dart';
 import 'package:shopfee/views/widgets/bottom_sheet.dart';
+import 'package:shopfee/views/widgets/info_card.dart';
+
+const url = "bryan123.me";
+const email = "bryan@gmail.com";
+const phone = "087888888888"; // not real number :)
+const location = "Jakarta, Indonesia";
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,15 +16,53 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        foregroundColor: textColor['heading'],
-        backgroundColor: neutral['light'],
-        title: const Text('Profile'),
-      ),
-      bottomSheet: const BottomSheetMenu(),
-    );
-  }
-}
+    backgroundColor: Colors.white,
+    bottomSheet: const BottomSheetMenu(),
+        body: SafeArea(
+          minimum: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/img/machiato.png'),
+              ),
+              Text(
+                "Bryan Ganteng",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "Members of Shopfee",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blueGrey[200],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Source Sans Pro"),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.brown,
+                ),
+              ),
+              InfoCard(text: phone, icon: Icons.phone, onPressed: () async {}),
+              InfoCard(text: url, icon: Icons.web, onPressed: () async {}),
+              InfoCard(
+                  text: location,
+                  icon: Icons.location_city,
+                  onPressed: () async {}),
+              InfoCard(text: email, icon: Icons.email, onPressed: () async {}),
+            ],
+          ),
+        ));
+  }}
